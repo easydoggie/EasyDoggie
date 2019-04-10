be324b42e7062659c92ecedd545ae81c8fe80477
sumomo-99 <sumomo99.create@gmail.com>
2019-04-10 01:02:03 +0000

    add phaser3 sample

--- node/src/test.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.write("<h1>Hello World</h1>");
-    res.end();
-}).listen(3000);
