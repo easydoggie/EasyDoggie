17ad6f15f1874c7ffc7b33994523cdaf219a421a
sumomo-99 <sumomo99.create@gmail.com>
2019-05-24 11:50:50 +0900

    Change nginx-unit image from original to official

--- nginx-unit-python/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-unitd --control unix:/var/run/control.unit.sock --log /var/log/unit.log
-
-curl -X PUT -d @/tmp/unit.conf.json --unix-socket /var/run/control.unit.sock http://localhost/config
-
-tail -f /var/log/unit.log
